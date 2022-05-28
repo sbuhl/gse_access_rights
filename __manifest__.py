c800fc57f8e3e1e54f6cb91a100915df7e0a1ac5
Sébastien Bühl <buhlsebastien@gmail.com>
2022-05-28 10:37:57 +0200

    [IMP] access_right: Referrer Only visible for all documents

--- __manifest__.py
+++ __manifest__.py
@@ -2,15 +2,15 @@
 
 {
     'name': "access_rights",
-    'version': '0.1',
-    'depends': ['base', 'l10n_be', 'account_accountant','sale', 'stock'],
+    'version': '0.2',
+    'depends': ['base', 'l10n_be', 'account_accountant', 'sale', 'stock', 'partner_commission'],
     
     'author': "Sébastien Bühl",
     'website': "http://www.buhl.be",
     'license': 'LGPL-3',
 
     'application': True,
-    'category': 'Other Extra Rights',
+    'category': 'Customizations',
     'summary': 'Allow access to other user documents',
     'description': """
         Access rights for invoices and other stuffs.
@@ -20,5 +20,6 @@
         'security/security.xml',
         'views/res_users.xml',
         'views/account_move.xml',
+        'views/sale_order.xml',
     ],
 }
