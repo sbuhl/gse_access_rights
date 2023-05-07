af0c01549d69c040fce626a5fd70297dda770228
shortcut1337 <really_mad@hotmail.com>
2023-05-07 22:58:55 +0200

    - la vue account.view_bank_statement_form qui n'existe plus, cf https://github.com/odoo/upgrade/blame/master/migrations/account/16.0.1.2/pre-migrate.py#L237 - Plus possible de faire <field name="groups_id" eval="[(4, ref('account.group_account_manager'))]"/> sur une vue héritée, il faut adapter tous les xpath à l'intérieur, cf https://github.com/odoo/upgrade/commit/b4375ad4c932e0f7938f5b6e7034edde2bc92ce4#diff-2e0dbcae3e1794f7c41dfbd02c6daea5f7c88277aad8d9fce7c602602cce7bfaR235 - Plus possible d'ajouter un groups à un field via xpath (comme tes modules font pour ajouter ton group custom à un field dans une vue odoo) si celui ci est utilisé ailleurs dans un invisible=(condition sur field) car maintenant groups sur un field le retire du DOM/de la vue si tu n'as pas le groupe, donc la computation de invisible qui dépend de lui ne peut pas se faire pour ceux qui n'ont pas le groupe, cf https://github.com/odoo/odoo/commit/0501bbd62e517f6c215d9e7e36d61747c7f5816b

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,11 @@
 {
     'name': "Access Rigths",
     'version': '0.8.2',
-    'depends': ['base', 'hr', 'l10n_be', 'account', 'account_accountant', 'sale', 'stock', 'partner_commission', 'industry_fsm', 'industry_fsm_sale_report'],
+    'depends': [
+        'base', 'hr', 'l10n_be', 'account', 'account_accountant', 'sale', 'stock', 'partner_commission', 'industry_fsm',
+        'industry_fsm_sale_report',
+        'account_approval',  # account_approval.group_sales_level_manager
+    ],
     'description': """
     Allow to specify access rights on users, journals and warehouses.
     """,
