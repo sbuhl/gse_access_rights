ef9e3e4baeff55b664b632ece7e41d930140e85b
shortcut1337 <really_mad@hotmail.com>
2023-08-21 01:46:07 +0200

    [FIX] prevent compute sudo access rule error
    `Access to Journal SBU` ir.rule is failing because
    `payment_method_line_id` is a stored compute (so `compute_sudo=True` by
    defaut.
    It ends up calling `_get_batch_available_journals` which does a
    `search()` on journals and bypass the `ir.rule` temporarily until the
    code return in "non sudo" mode and crash.

---
+++ models/account_payment_register.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models
+
+
+class AccountPaymentRegister(models.TransientModel):
+    _inherit = 'account.payment.register'
+
+    @api.model
+    def _get_batch_available_journals(self, batch_result):
+        return super(
+            AccountPaymentRegister, self.with_user(self.env.user),
+        )._get_batch_available_journals(batch_result)
