71dac87bb4c8b6eb703db1bee48dde0376c86e52
Sébastien Bühl <buhlsebastien@gmail.com>
2023-09-01 14:31:27 +0200

    [FIX] outstanding credits widget only for accountant

--- models/account_move.py
+++ models/account_move.py
@@ -14,3 +14,19 @@ class AccountMove(models.Model):
         for record in self:
             record.sudo()._compute_payments_widget_reconciled_info()
             record['invoice_payment_wigdet_journal'] = record.invoice_payments_widget
+    
+    # === Payment widget fields === #
+    invoice_outstanding_credits_debits_widget = fields.Binary(
+        groups="account.group_account_manager",
+        compute='_compute_payments_widget_to_reconcile_info',
+        exportable=False,
+    )
+    invoice_has_outstanding = fields.Boolean(
+        groups="account.group_account_manager",
+        compute='_compute_payments_widget_to_reconcile_info',
+    )
+    invoice_payments_widget = fields.Binary(
+        groups="account.group_account_manager",
+        compute='_compute_payments_widget_reconciled_info',
+        exportable=False,
+    )
