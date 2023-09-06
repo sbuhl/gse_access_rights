de5203ec6b1748981de0b53e41fd7b7f2b1f974e
shortcut1337 <really_mad@hotmail.com>
2023-09-06 23:18:58 +0200

    [IMP] allow to add note on product
    normally you need write access

--- models/__init__.py
+++ models/__init__.py
@@ -4,5 +4,6 @@ from . import users
 from . import account_journal
 from . import account_move
 from . import account_payment_register
+from . import mail_thread
 from . import stock_location
 from . import stock_warehouse
