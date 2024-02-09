b289b10ffb72c7fa49a361c5d142d1758d2ced68
assia <johndoe@example.com>
2024-02-09 14:15:01 +0200

    [ADD] install a linter

--- models/account_journal.py
+++ models/account_journal.py
@@ -6,4 +6,4 @@ from odoo import models, fields
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
-    sbu_allowed_user_ids = fields.Many2many('res.users', string="Allowed Users")
+    sbu_allowed_user_ids = fields.Many2many("res.users", string="Allowed Users")
--- models/account_move.py
+++ models/account_move.py
@@ -5,28 +5,30 @@ from odoo import models, fields, api
 
 class AccountMove(models.Model):
 
-    _inherit = 'account.move'
+    _inherit = "account.move"
 
-    invoice_payment_wigdet_journal = fields.Char(string="Invoice Payments", compute="_get_payment_widget")
+    invoice_payment_wigdet_journal = fields.Char(
+        string="Invoice Payments", compute="_get_payment_widget"
+    )
 
-    @api.depends('move_type', 'line_ids.amount_residual')
+    @api.depends("move_type", "line_ids.amount_residual")
     def _get_payment_widget(self):
         for record in self:
             record.sudo()._compute_payments_widget_reconciled_info()
-            record['invoice_payment_wigdet_journal'] = record.invoice_payments_widget
+            record["invoice_payment_wigdet_journal"] = record.invoice_payments_widget
 
     # === Payment widget fields === #
     invoice_outstanding_credits_debits_widget = fields.Binary(
         groups="account.group_account_manager",
-        compute='_compute_payments_widget_to_reconcile_info',
+        compute="_compute_payments_widget_to_reconcile_info",
         exportable=False,
     )
     invoice_has_outstanding = fields.Boolean(
         groups="account.group_account_manager",
-        compute='_compute_payments_widget_to_reconcile_info',
+        compute="_compute_payments_widget_to_reconcile_info",
     )
     invoice_payments_widget = fields.Binary(
         groups="account.group_account_manager",
-        compute='_compute_payments_widget_reconciled_info',
+        compute="_compute_payments_widget_reconciled_info",
         exportable=False,
     )
