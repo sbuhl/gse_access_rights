4d50fdd7912b07bf80a52af4e2e72830d1a6657e
Assiati Magana <assia.magana@gmail.com>
2024-03-20 13:52:33 +0200

    [IMP]gse_access_rights: Create Product Access Right

--- models/stock_location.py
+++ models/stock_location.py
@@ -4,6 +4,6 @@ from odoo import models, fields
 
 
 class StockLocation(models.Model):
-    _inherit = "stock.location"
+    _inherit = 'stock.location'
 
     sbu_allowed_warehouses_id = fields.Many2one(comodel_name='stock.warehouse', store=True)  # noqa: E501
